57886e49acbe8d1e93b00d48900d207c533ce233
Jainil Gupta <jainilgupta93@gmail.com>
2026-06-07 13:26:24 +0530

    Refactor app.js to improve route and logging

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello from ECS! 🚀  v1'));
-app.get('/', (req, res) => res.send('Now New commit and Release! 🚀  v2'));
+app.get('/', (req, res) => {
+  res.send('Now New commit and Release! 🚀 v2');
+});
 
-app.listen(port, () => console.log(`App running on port ${port}`));
-app.listen(port, () => console.log(`App running on port ${port}`));
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
